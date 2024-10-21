fc5f72fce5c3c07f316b5888b88a3c1b1e3e5df5
Mirco Meiners <Mirco.Meiners@hs-bremen.de>
2024-10-21 13:18:23 +0200

    Clean up

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-1# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 #
 # Configuration file for the Sphinx documentation builder.
 #
@@ -46,7 +46,7 @@ extensions = [
     'sphinx.ext.githubpages', 'sphinxcontrib.bibtex'
 ]
 
-bibtex_bibfiles = ['refs.bib']
+bibtex_bibfiles = ['docs/refs.bib']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -68,9 +68,9 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-#language = u'de'
-#locale_dirs = ['locale/']
-#gettext_compact = False
+language = u'de'
+locale_dirs = ['locale/']
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -204,7 +204,7 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # -- Options for todo extension ----------------------------------------------
 
